f8f652295fea292b147f16673ab5d6487fbc67ed
mung-happy <mungnxhnv7@gmail.com>
2025-02-28 22:16:06 +0700

    update

--- src/app/(client)/artists/page.tsx
+++ src/app/(client)/artists/page.tsx
@@ -1,10 +1,11 @@
 import SectionContainer from "@/components/SectionList/SectionContainer";
 import SectionItem from "@/components/SectionList/SectionItem";
 import { getArtist } from "@/services/Artists";
+import { DetailData } from "@/types/ultits.type";
 
 export default async function PageArtist() {
-  const artists = await getArtist();
-
+  // const artists = await getArtist();
+  const artists: DetailData[] = [];
   return (
     <SectionContainer title="Nghệ sĩ nổi bật" link="">
       {artists.map((artist) => (
--- src/app/(client)/tracks/page.tsx
+++ src/app/(client)/tracks/page.tsx
@@ -2,9 +2,11 @@ import { TrackType } from "@/types/track.type";
 import SectionItem from "@/components/SectionList/SectionItem";
 import { getTracks } from "@/services/Tracks";
 import SectionContainer from "@/components/SectionList/SectionContainer";
+import { DetailData } from "@/types/ultits.type";
 
 export default async function Pagetrack() {
-  const data = await getTracks();
+  // const data = await getTracks();
+  const data: DetailData[] = [];
 
   return (
     <SectionContainer title="Bài hát nổi bật" link="">
